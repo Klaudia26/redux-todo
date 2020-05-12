8d292014e30364f30f820aabe417adb59cfd6010
Klaudia <klaudia.krzywda@gmail.com>
2020-05-12 11:48:34 +0100

    Updates

---
+++ precache-manifest.11522382738621a560eab672cdfa6beb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76fc99ccc7674f513040ad520df359b",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "e8108160b02ac536fbaa",
+    "url": "/redux-todo/static/css/main.35f44d1a.chunk.css"
+  },
+  {
+    "revision": "1a6fe1f95ce79e47277f",
+    "url": "/redux-todo/static/js/2.38f3661d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-todo/static/js/2.38f3661d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8108160b02ac536fbaa",
+    "url": "/redux-todo/static/js/main.d4668996.chunk.js"
+  },
+  {
+    "revision": "6dd3e1f2aeaca1f0ce24",
+    "url": "/redux-todo/static/js/runtime-main.dc7c703e.js"
+  }
+]);
